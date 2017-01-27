c73891a29595c53e355c1d1da8ba0cdb493aaf7d
Nicola Paolucci <naeemhossain.mail@gmail.com>
2017-01-27 18:56:43 -0500

    figuring schtuff out fam

--- Asst0/pointersorter.c
+++ Asst0/pointersorter.c
@@ -18,10 +18,11 @@
 // 5) Sorting
 // 6) Return result
 
+/* Prototype read input method
 int main(){
 
 	FILE *f;
-    char c;s
+    char c;
     f=fopen("testcases.txt","rt");
 
     while((c=fgetc(f))!=EOF){
@@ -31,4 +32,24 @@ int main(){
     fclose(f);
     return 0;
 }
+*/
+
+int main(int argc, char* argv[])
+{
+    char const* const testcases = argv[1]; /* should check that argc > 1 */
+    FILE* file = fopen("testcases.txt", "r"); /* should check the result */
+    char line[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", line); 
+    }
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
+
+    fclose(file);
+    return 0;
+}
+
 
