0d939cc7da2486baca610053b2876af260ca241b
Naeem Hossain <nh302@rm.cs.rutgers.edu>
2017-02-02 22:57:01 -0500

    revised file, will implement using a linked list instead of a regular file

--- Asst0/pointersorter.c
+++ Asst0/pointersorter.c
@@ -3,10 +3,10 @@
 // February 5, 2017
 // Asst0: String Sorting
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+// You will write a rather simple program to sort words given in a 
+// single input string. Your code will take a single, long input string 
+// that contains all words to be sorted, break them out into individual strings,
+// sort them, and output them one per line in descending alphabetical order.
 //Separated by non alphabet chars
 
 // 1) Create empty hashtable
@@ -16,10 +16,47 @@
 // 5) Sorting
 // 6) Return result
 
-// You will write a rather simple program to sort words given in a 
-// single input string. Your code will take a single, long input string 
-// that contains all words to be sorted, break them out into individual strings,
-// sort them, and output them one per line in descending alphabetical order.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//char c; //global var
+
+
+struct node{
+
+    char name[20];
+    int id;
+    struct node *next;
+
+}*head;
+
+
+
+int main(void){
+
+    struct node *temp;
+    temp = malloc(sizeof(struct node));
+    temp->next = NULL;
+    head = temp;
+
+    FILE *ifp;
+    ifp = fopen("testcases.txt", "r");
+
+    int c = 0;
+
+    char buffer[1024];
+    memset(buffer, 0, 1024);
+    while(c<5){
+        fgets(buffer, 1024, ifp);
+        sscanf(buffer, "%19[^,], %d", temp->name, &temp->id);
+        printf("%d %s %d\n",c, temp->name, temp->id);
+        temp->next = malloc(sizeof(struct node));
+        temp = temp->next;
+        temp->next = NULL;
+        c++;
+    }
+}
 
 /*
 int main(int argc, char* argv[])
@@ -41,9 +78,35 @@ int main(int argc, char* argv[])
 }
 */
 
-int main(int argc, char* argv []){
-
+/*
+int main()
+{
+	// Creating a pointer file type
+	FILE *ptr;
+	char array[255];
+	int id;
+	int i;
+	//EOF = "END_OF_FILE"
 	
+	// Opening a file for reading data
+	ptr = fopen("testcases.txt", "r");
+	
+	for(i = 0; i <= EOF; i++)
+	{
+		// Reading data from the file
+		fscanf(ptr, "%s", array); 
+	
+		// Concerting string to integer using "atoi" function
+		id = atoi(array);
+	
+		// Check if id is 2
+		if (id == 4) 
+		{
+			fgets(array, 255, (FILE*)ptr); //used to read the whole line
+			printf("%s", array);
+			break; //while condition match then terminate loop
+		}
+	}
+	return 0;
 }
-
-
+*/
