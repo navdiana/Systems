4b34c2104e7ddf60a6f8bed35e13aab6c47d4f55
Nicola Paolucci <naeemhossain.mail@gmail.com>
2017-02-02 20:20:21 -0500

    SwagMoney my life sucks

--- Asst0/pointersorter.c
+++ Asst0/pointersorter.c
@@ -5,11 +5,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
+#include <string.h>
 
-// Simple program to sort words from a given input string: 
-
-// NON ALPHABET CHARS
+//Separated by non alphabet chars
 
 // 1) Create empty hashtable
 // 2) Go through string, turn all non-alphabet chars into spaces
@@ -18,38 +16,34 @@
 // 5) Sorting
 // 6) Return result
 
-/* Prototype read input method
-int main(){
+// You will write a rather simple program to sort words given in a 
+// single input string. Your code will take a single, long input string 
+// that contains all words to be sorted, break them out into individual strings,
+// sort them, and output them one per line in descending alphabetical order.
 
-	FILE *f;
-    char c;
-    f=fopen("testcases.txt","rt");
+/*
+int main(int argc, char* argv[])
+{
+    char const* const testcases = argv[1]; // should check that argc > 1 
+    FILE* file = fopen("testcases.txt", "r"); // should check the result 
+    char line[256];
 
-    while((c=fgetc(f))!=EOF){
-        printf("%c",c);
+    while (fgets(line, sizeof(line), file)) {
+        // note that fgets don't strip the terminating \n, checking its
+        // presence would allow to handle lines longer that sizeof(line) 
+        printf("%s\n", line); 
     }
+    //may check feof here to make a difference between eof and io failure -- network
+    //timeout for instance 
 
-    fclose(f);
+    fclose(file);
     return 0;
 }
 */
 
-int main(int argc, char* argv[])
-{
-    char const* const testcases = argv[1]; /* should check that argc > 1 */
-    FILE* file = fopen("testcases.txt", "r"); /* should check the result */
-    char line[256];
+int main(int argc, char* argv []){
 
-    while (fgets(line, sizeof(line), file)) {
-        /* note that fgets don't strip the terminating \n, checking its
-           presence would allow to handle lines longer that sizeof(line) */
-        printf("%s", line); 
-    }
-    /* may check feof here to make a difference between eof and io failure -- network
-       timeout for instance */
-
-    fclose(file);
-    return 0;
+	
 }
 
 
